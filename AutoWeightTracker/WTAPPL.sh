ea0f42332cc042a7fc7eb54c6aee190d1cb3754b
Ron Kaiser <70015659+ronkaiser@users.noreply.github.com>
2021-04-19 22:01:42 +0300

    dependencies installation

--- AutoWeightTracker/WTAPPL.sh
+++ AutoWeightTracker/WTAPPL.sh
@@ -6,6 +6,8 @@
 sudo apt update
 sudo apt install nodejs -y
 sudo apt install npm -y
+sudo npm install @hapi/hapi@19 @hapi/bell@12 @hapi/boom@9 @hapi/cookie@11 @hapi/inert@6 @hapi/joi@17 @hapi/vision@6 dotenv@8 ejs@3 postgres@1 -y
+sudo npm install --save-dev nodemon@2 -y
 
 # clone repository using git
 git clone https://github.com/ronkaiser/node-weight-tracker.git
