117500333682f935ef96368ce7d71577c9a0510b
Ron Kaiser <70015659+ronkaiser@users.noreply.github.com>
2021-04-20 23:48:31 +0300

    Update WTAPPL.sh

--- AutoWeightTracker/WTAPPL.sh
+++ AutoWeightTracker/WTAPPL.sh
@@ -10,10 +10,11 @@ node -v
 
 # clone repository using git
 git clone https://github.com/ronkaiser/node-weight-tracker.git
-cd ~/node-weight-tracker/
 sudo npm init -y
 sudo npm install @hapi/hapi@19 @hapi/bell@12 @hapi/boom@9 @hapi/cookie@11 @hapi/inert@6 @hapi/joi@17 @hapi/vision@6 dotenv@8 ejs@3 postgres@1 -y
 sudo npm install --save-dev nodemon@2 -y
 
 # pm2 install
 sudo npm install pm2@latest -g
+
+# env edit
